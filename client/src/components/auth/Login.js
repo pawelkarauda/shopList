e7f30cafb50c86dbed2b3d6bf59ac1f9398f7227
unknown <zlapmniehakerze@gmail.com>
2018-08-16 16:36:08 +0200

    Little change

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -5,108 +5,106 @@ import { loginUser } from "../../actions/authActions";
 import TextFieldGroup from "../common/TextFieldGroup";
 
 class Login extends Component {
-    constructor() {
-        super();
-        this.state = {
-            email: "",
-            password: "",
-            errors: {}
-        };
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      errors: {}
+    };
 
-        this.onChange = this.onChange.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  componentWillMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
+  }
+
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.auth.isAuthenticated) {
+      this.props.history.push("/shoplist");
     }
 
-    componentWillMount() {
-        if (this.props.auth.isAuthenticated) {
-            this.props.history.push("/dashboard");
-        }
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
     }
+  }
 
-    componentDidMount() {
-        if (this.props.auth.isAuthenticated) {
-            this.props.history.push("/dashboard");
-        }
-    }
+  onSubmit(e) {
+    e.preventDefault();
 
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.auth.isAuthenticated) {
-            this.props.history.push("/shoplist");
-        }
+    const userData = {
+      email: this.state.email,
+      password: this.state.password
+    };
 
-        if (nextProps.errors) {
-            this.setState({ errors: nextProps.errors });
-        }
-    }
+    this.props.loginUser(userData);
+  }
 
-    onSubmit(e) {
-        e.preventDefault();
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
 
-        const userData = {
-            email: this.state.email,
-            password: this.state.password
-        };
+  render() {
+    const { errors } = this.state;
 
-        this.props.loginUser(userData);
-    }
+    return (
+      <div className="login">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Log In</h1>
 
-    onChange(e) {
-        this.setState({ [e.target.name]: e.target.value });
-    }
+              <form onSubmit={this.onSubmit}>
+                <TextFieldGroup
+                  placeholder="Email addres"
+                  name="email"
+                  type="email"
+                  value={this.state.email}
+                  onChange={this.onChange}
+                  error={errors.email}
+                />
 
-    render() {
-        const { errors } = this.state;
+                <TextFieldGroup
+                  placeholder="Password"
+                  name="password"
+                  type="password"
+                  value={this.state.password}
+                  onChange={this.onChange}
+                  error={errors.password}
+                />
 
-        return (
-            <div className="login">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-8 m-auto">
-                            <h1 className="display-4 text-center">Log In</h1>
-                            <p className="lead text-center">
-                                Sign in to your DevConnector account
-                            </p>
-                            <form onSubmit={this.onSubmit}>
-                                <TextFieldGroup
-                                    placeholder="Email addres"
-                                    name="email"
-                                    type="email"
-                                    value={this.state.email}
-                                    onChange={this.onChange}
-                                    error={errors.email}
-                                />
-
-                                <TextFieldGroup
-                                    placeholder="Password"
-                                    name="password"
-                                    type="password"
-                                    value={this.state.password}
-                                    onChange={this.onChange}
-                                    error={errors.password}
-                                />
-
-                                <input
-                                    type="submit"
-                                    className="btn btn-info btn-block mt-4"
-                                />
-                            </form>
-                        </div>
-                    </div>
-                </div>
+                <input type="submit" className="btn btn-info btn-block mt-4" />
+              </form>
             </div>
-        );
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 Login.propTypes = {
-    loginUser: PropTypes.func.isRequired,
-    auth: PropTypes.object.isRequired,
-    errors: PropTypes.object.isRequired
+  loginUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
-    auth: state.auth,
-    errors: state.errors
+  auth: state.auth,
+  errors: state.errors
 });
 
-export default connect(mapStateToProps, { loginUser })(Login);
+export default connect(
+  mapStateToProps,
+  { loginUser }
+)(Login);
